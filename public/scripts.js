e724fc1277325a087c21eaf0b8682c7a827745ed
Just-Mia3 <134561818+Just-Mia3@users.noreply.github.com>
2025-09-15 22:05:06 -0500

    Update scripts.js
    I believe I've fixed it

--- public/scripts.js
+++ public/scripts.js
@@ -1,5 +1,7 @@
 const mySound = new Audio("https://files.catbox.moe/pfgfy2.mp3");
 const myImage = document.getElementById("autism");
 
-myImage.addEventListener("click" ,() => {mySound.currentTime = 0; mySound.play}();
+myImage.addEventListener("click", () => {
+  mySound.currentTime = 0; 
+  mySound.play();
 });
