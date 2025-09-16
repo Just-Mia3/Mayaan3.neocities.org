3cb2719147cfd8186e9a00a71ae91c635bc1a5d6
Just-Mia3 <134561818+Just-Mia3@users.noreply.github.com>
2025-09-15 21:54:17 -0500

    Update scripts.js
    now the sound should play

--- public/scripts.js
+++ public/scripts.js
@@ -1,4 +1,4 @@
-const mySound = new Audio("duck.mp3");
+const mySound = new Audio("https://files.catbox.moe/pfgfy2.mp3");
 const myImage = document.getElementById("autism");
 
 myImage.addEventListener("click" ,() => {mySound.currentTime = 0; mySound.play}();
